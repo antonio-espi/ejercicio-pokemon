21d3949b042162f8b47d3fa63b68d85d6b460b84
antonio-espi <antonio.e.negrete@gmail.com>
2023-08-23 21:36:45 -0600

    segundo comit

--- main.js
+++ main.js
@@ -1,20 +1,55 @@
-const apiButton = document.getElementById('apiButton');
-const apiData = document.getElementById('apiData');
-const count = document.getElementById('count');
-const next = document.getElementById('next');
-const previous = document.getElementById('previous');
-const results = document.getElementById('results');
+const lists__pokemons = document.getElementById('lists__pokemons')
+const buttons = document.getElementById('buttons')
+let urlPokemon = ' https://pokeapi.co/api/v2/pokemon'
+let btnNext;
+let btnPrevious;
+let templateHtml;
 
-const callAPI = () => {
-    fetch ('https://pokeapi.co/api/v2/pokemon/')
-        .then(res => res.json())
-        .then(data => {
-            count.innerText = `${JSON.stringify(data.count)}`
-            next.innerText = `${JSON.stringify(data.next)}`
-            previous.innerText = `${JSON.stringify(data.previous)}`
-            results.innerText = `${JSON.stringify(data.results)}`
-        })
-        .catch(e => console.error(new Error(e)));
+
+const GetPokemons = async (url) => {
+    try {
+        const response = await fetch(url)
+        const results = await response.json();
+        console.log(results)
+        DataPokemons(results.results)
+
+        btnNext=results.next ? `<button class="btn" data-url=${results.next}>Sigiente</button>` : ''
+        btnPrevious=results.previous ? `<button class="btn" data-url=${results.previous}>Atras</button>` : ''
+        buttons.innerHTML=btnPrevious + " " + btnNext
+        
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+GetPokemons(urlPokemon)
+
+const DataPokemons = async (data) => {
+    lists__pokemons.innerHTML = '';
+    try {
+        for (let index of data) {
+
+            const resp = await fetch(index.url)
+            const resul = await resp.json();
+            console.log(resul)
+            templateHtml=`
+            <div class="pokemon__img">
+            <img src=${resul.sprites.other.dream_world.front_default} alt=${resul.name}/>
+            <p>${resul.name}</p>
+            </div>
+            `
+            lists__pokemons.innerHTML+=templateHtml
+        }
+        
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-apiButton.addEventListener('click', callAPI);
+buttons.addEventListener('click',(e)=>{
+    if(e.target.matches('.btn')){
+        let value=e.target.dataset.url
+        console.log(value)
+        GetPokemons(value)
+    }
+})
